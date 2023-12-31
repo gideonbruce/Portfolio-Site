9884c0f6d761c5f2f644a8ebda83257294e4cc63
bruce <bruceotieno25@gmail.com>
2023-12-31 16:24:08 +0300

    code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/gideonotieno"
+                href="https://github.com/gideonbruce"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
